4832ed7515d9e3dbf104edfc3072b4e5f8a6e5f5
Shivansh2287 <54775060+Shivansh2287@users.noreply.github.com>
2020-10-31 02:34:51 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/app'
+
+
+require('firebase/firestore')
+  var firebaseConfig = {
+    apiKey: "AIzaSyBDKp7BzxVRHM7AFpsyieyG36zA59Ah3eg",
+    authDomain: "evernote-cb0f1.firebaseapp.com",
+    databaseURL: "https://evernote-cb0f1.firebaseio.com",
+    projectId: "evernote-cb0f1",
+    storageBucket: "evernote-cb0f1.appspot.com",
+    messagingSenderId: "498359815722",
+    appId: "1:498359815722:web:43da31c91254e39855fd53",
+    measurementId: "G-LX00KJZZQC"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
 
 ReactDOM.render(
   <React.StrictMode>
